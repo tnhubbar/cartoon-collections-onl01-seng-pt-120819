6e12745cc99b583c926289142fbeda589f5b2ea5
Taryn Johnson <taryn.johnson919@gmail.com>
2019-12-18 03:10:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,10 +19,9 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  cheeses = ["gouda" "cheddar" "camembert"]
+  cheeses = %w[gouda cheddar camembert]
 
-  array.find do |cheese|
-    cheeses.include?(cheese )
+  array.find do |maybe_cheese|
+    cheeses.include?(maybe_cheese)
   end
-  find_the_cheese(array)
 end
