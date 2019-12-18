b89c1a54201b9796c6a4fd9332440adce5496b7f
Taryn Johnson <taryn.johnson919@gmail.com>
2019-12-18 03:06:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,7 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  array.include? "cheddar"
+  array.include? ("cheddar")
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
